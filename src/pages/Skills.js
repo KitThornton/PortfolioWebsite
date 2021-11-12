6ab79f73aca782ba071b5745235209ee9c60ca46
kitthornton <kit.thornton16@gmail.com>
2021-11-12 10:56:14 +0000

    refine skills and avatar

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -9,6 +9,7 @@ export const Skills = () => {
             <Row>
                 <Col className="skills-text">
                     Here's a breakdown of my professional skills acquired through both commercial and personal use.
+
                 </Col>
             </Row>
             <Row>
@@ -19,7 +20,10 @@ export const Skills = () => {
                     <StarRatingWithDescription rating={4} description="React" />
                     <StarRatingWithDescription rating={4} description="Bootstrap" />
                     <StarRatingWithDescription rating={2} description="Redux" />
-                    <StarRatingWithDescription rating={4} description="Bootstrap" />
+                    <StarRatingWithDescription rating={3} description="Javascript" />
+                    <StarRatingWithDescription rating={3} description="CSS" />
+                    <StarRatingWithDescription rating={3} description="HTML" />
+                    <StarRatingWithDescription rating={1} description="Angular" />
                 </Col>
                 <Col xs={12} md={6}>
                     <div className="skills-header">
@@ -27,9 +31,10 @@ export const Skills = () => {
                     </div>
                     <StarRatingWithDescription rating={4} description="C#" />
                     <StarRatingWithDescription rating={4} description="Node.js" />
-                    <StarRatingWithDescription rating={2} description=".NET Core" />
+                    <StarRatingWithDescription rating={3} description=".NET Core" />
                     <StarRatingWithDescription rating={2} description=".NET MVC" />
                     <StarRatingWithDescription rating={2} description="VB.NET" />
+                    <StarRatingWithDescription rating={1} description="Python" />
                 </Col>
             </Row>
             <Row>
@@ -39,6 +44,7 @@ export const Skills = () => {
                     </div>
                     <StarRatingWithDescription rating={4} description="MS SQL Server" />
                     <StarRatingWithDescription rating={3} description="PostgreSQL" />
+                    <StarRatingWithDescription rating={2} description="Azure Data Studio" />
                 </Col>
                 <Col xs={12} md={6}>
                     <div className="skills-header">
@@ -48,6 +54,7 @@ export const Skills = () => {
                     <StarRatingWithDescription rating={3} description="Netlify" />
                     <StarRatingWithDescription rating={2} description="AWS" />
                     <StarRatingWithDescription rating={3} description="Team City" />
+                    <StarRatingWithDescription rating={2} description="Team Foundation Server" />
                 </Col>
             </Row>
             <Row>
@@ -57,6 +64,7 @@ export const Skills = () => {
                     </div>
                     <StarRatingWithDescription rating={3} description="GitHub" />
                     <StarRatingWithDescription rating={3} description="Agile" />
+                    <StarRatingWithDescription rating={2} description="Scrum" />
                     <StarRatingWithDescription rating={2} description="Jira" />
                 </Col>
                 <Col>
@@ -68,17 +76,33 @@ export const Skills = () => {
                     <StarRatingWithDescription rating={2} description="Jest" />
                 </Col>
             </Row>
+            <Row>
+                <Col xs={12} md={6}>
+                    <div className="skills-header">
+                        Tools
+                    </div>
+                    <StarRatingWithDescription rating={4} description="Visual Studio" />
+                    <StarRatingWithDescription rating={4} description="JetBrains products" />
+                    <StarRatingWithDescription rating={4} description="Resharper" />
+                </Col>
+                <Col>
+                    <div className="skills-header">
+                        Testing
+                    </div>
+                    <StarRatingWithDescription rating={3} description="NUnit" />
+                </Col>
+            </Row>
         </Container>
     )
 }
 
 const StarRatingWithDescription = (props) => {
     return (
-        <Row className="skills-description-row">
+        <Row className="skills-description-row rounded-1">
             <Col className="skills-description my-auto">
                 {props.description}
             </Col>
-            <Col className="my-auto">
+            <Col className="star-rating my-auto">
                 <StarRating5Max rating={props.rating} />
             </Col>
         </Row>
@@ -88,8 +112,9 @@ const StarRatingWithDescription = (props) => {
 const StarRating5Max = (props) => {
     return (
         <ReactStars
+            classNames="star"
             count={5}
-            size={24}
+            size={20}
             value ={props.rating}
             activeColor="#ffd700"
             edit={false}
